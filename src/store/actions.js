1300528fa7d8ab604e5fac23e14cfaa69d1be4e3
David <djsfdavid@gmail.com>
2019-09-19 14:45:46 +0200

    Creates orders - buy and sell

--- src/store/actions.js
+++ src/store/actions.js
@@ -144,4 +144,53 @@ return {
     }
 }
 
+//Creating Orders
+//...Buy Orders
+export function buyOrderAmountChanged(amount) {
+    return {
+      type: 'BUY_ORDER_AMOUNT_CHANGED',
+      amount
+    }
+  }
+  
+  export function buyOrderPriceChanged(price) {
+    return {
+      type: 'BUY_ORDER_PRICE_CHANGED',
+      price
+    }
+  }
+  
+  export function buyOrderMaking(price) {
+    return {
+      type: 'BUY_ORDER_MAKING'
+    }
+  }
+// Generic Order (both buy and sell)
+export function orderMade(order) {
+    return {
+      type: 'ORDER_MADE',
+      order
+    }
+  }
 
+// ...Sell Order
+export function sellOrderAmountChanged(amount) {
+    return {
+      type: 'SELL_ORDER_AMOUNT_CHANGED',
+      amount
+    }
+  }
+  
+  export function sellOrderPriceChanged(price) {
+    return {
+      type: 'SELL_ORDER_PRICE_CHANGED',
+      price
+    }
+  }
+  
+  export function sellOrderMaking(price) {
+    return {
+      type: 'SELL_ORDER_MAKING'
+    }
+  }
+  
